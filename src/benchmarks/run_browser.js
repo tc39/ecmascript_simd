b13b9aa98dd2e3ba777c3c7882accfe673988b96
John McCutchan <john@johnmccutchan.com>
2013-09-30 18:30:36 -0700

    Add browser benchmark harness

---
+++ src/benchmarks/run_browser.js
@@ -0,0 +1,19 @@
+function printResult(str) {
+  console.log(str);
+}
+
+function printError(str) {
+  console.log(str);
+}
+
+function printScore(str) {
+  console.log(str);
+}
+
+window.onload = function() {
+  console.log('Running benchmarks.');
+  benchmarks.runAll({notifyResult: printResult,
+                     notifyError:  printError,
+                     notifyScore:  printScore});
+  console.log('Benchmarks completed.');
+}
