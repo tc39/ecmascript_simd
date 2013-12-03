1c3b34478d31c943fa4b4a411faf7632a52148e8
Peter Jensen <peter.jensen@intel.com>
2013-12-03 15:35:51 -0800

    Inlined kernel functions to avoid measuring call overhead when JS engine doesn't do the inlining

--- src/benchmarks/inverse4x4.js
+++ src/benchmarks/inverse4x4.js
@@ -305,15 +305,212 @@
 
   // SIMD version of the kernel
   function simdMatrixInverseN(n) {
-    for (var i = 0; i < n; ++i) {
-      simdMatrixInverse ();
+    for (var iterations = 0; iterations < n; ++iterations) {
+      var src0, src1, src2, src3;
+      var row0, row1, row2, row3;
+      var tmp1;
+      var minor0, minor1, minor2, minor3;
+      var det;
+
+      // Load the 4 rows
+      var src0 = srcx4.getAt(0);
+      var src1 = srcx4.getAt(1);
+      var src2 = srcx4.getAt(2);
+      var src3 = srcx4.getAt(3);
+
+      // Transpose the source matrix.  Sort of.  Not a true transpose operation
+
+      tmp1 = SIMD.float32x4.shuffleMix(src0, src1, SIMD.XYXY);
+      row1 = SIMD.float32x4.shuffleMix(src2, src3, SIMD.XYXY);
+      row0 = SIMD.float32x4.shuffleMix(tmp1, row1, SIMD.XZXZ);
+      row1 = SIMD.float32x4.shuffleMix(row1, tmp1, SIMD.YWYW);
+
+      tmp1 = SIMD.float32x4.shuffleMix(src0, src1, SIMD.ZWZW);
+      row3 = SIMD.float32x4.shuffleMix(src2, src3, SIMD.ZWZW);
+      row2 = SIMD.float32x4.shuffleMix(tmp1, row3, SIMD.XZXZ);
+      row3 = SIMD.float32x4.shuffleMix(row3, tmp1, SIMD.YWYW);
+
+      // This is a true transposition, but it will lead to an incorrect result
+
+      //tmp1 = SIMD.float32x4.shuffleMix(src0, src1, SIMD.XYXY);
+      //tmp2 = SIMD.float32x4.shuffleMix(src2, src3, SIMD.XYXY);
+      //row0  = SIMD.float32x4.shuffleMix(tmp1, tmp2, SIMD.XZXZ);
+      //row1  = SIMD.float32x4.shuffleMix(tmp1, tmp2, SIMD.YWYW);
+
+      //tmp1 = SIMD.float32x4.shuffleMix(src0, src1, SIMD.ZWZW);
+      //tmp2 = SIMD.float32x4.shuffleMix(src2, src3, SIMD.ZWZW);
+      //row2  = SIMD.float32x4.shuffleMix(tmp1, tmp2, SIMD.XZXZ);
+      //row3  = SIMD.float32x4.shuffleMix(tmp1, tmp2, SIMD.YWYW);
+
+      // ----
+      tmp1 = SIMD.float32x4.mul(row2, row3);
+      tmp1 = SIMD.float32x4.shuffle(tmp1, SIMD.YXWZ); // 0xB1 = 10110001
+      minor0 = SIMD.float32x4.mul(row1, tmp1);
+      minor1 = SIMD.float32x4.mul(row0, tmp1);
+      tmp1 = SIMD.float32x4.shuffle(tmp1, SIMD.ZWXY); // 0x4E = 01001110
+      minor0 = SIMD.float32x4.sub(SIMD.float32x4.mul(row1, tmp1), minor0);
+      minor1 = SIMD.float32x4.sub(SIMD.float32x4.mul(row0, tmp1), minor1);
+      minor1 = SIMD.float32x4.shuffle(minor1, SIMD.ZWXY); // 0x4E = 01001110
+
+      // ----
+      tmp1 = SIMD.float32x4.mul(row1, row2);
+      tmp1 = SIMD.float32x4.shuffle(tmp1, SIMD.YXWZ); // 0xB1 = 10110001
+      minor0 = SIMD.float32x4.add(SIMD.float32x4.mul(row3, tmp1), minor0);
+      minor3 = SIMD.float32x4.mul(row0, tmp1);
+      tmp1 = SIMD.float32x4.shuffle(tmp1, SIMD.ZWXY); // 0x4E = 01001110
+      minor0 = SIMD.float32x4.sub(minor0, SIMD.float32x4.mul(row3, tmp1));
+      minor3 = SIMD.float32x4.sub(SIMD.float32x4.mul(row0, tmp1), minor3);
+      minor3 = SIMD.float32x4.shuffle(minor3, SIMD.ZWXY); // 0x4E = 01001110
+
+      // ----
+      tmp1 = SIMD.float32x4.mul(SIMD.float32x4.shuffle(row1, SIMD.ZWXY), row3); // 0x4E = 01001110
+      tmp1 = SIMD.float32x4.shuffle(tmp1, SIMD.YXWZ); // 0xB1 = 10110001
+      row2 = SIMD.float32x4.shuffle(row2, SIMD.ZWXY);  // 0x4E = 01001110
+      minor0 = SIMD.float32x4.add(SIMD.float32x4.mul(row2, tmp1), minor0);
+      minor2 = SIMD.float32x4.mul(row0, tmp1);
+      tmp1 = SIMD.float32x4.shuffle(tmp1, SIMD.ZWXY); // 0x4E = 01001110
+      minor0 = SIMD.float32x4.sub(minor0, SIMD.float32x4.mul(row2, tmp1));
+      minor2 = SIMD.float32x4.sub(SIMD.float32x4.mul(row0, tmp1), minor2);
+      minor2 = SIMD.float32x4.shuffle(minor2, SIMD.ZWXY); // 0x4E = 01001110
+
+      // ----
+      tmp1 = SIMD.float32x4.mul(row0, row1);
+      tmp1 = SIMD.float32x4.shuffle(tmp1, SIMD.YXWZ); // 0xB1 = 10110001
+      minor2 = SIMD.float32x4.add(SIMD.float32x4.mul(row3, tmp1), minor2);
+      minor3 = SIMD.float32x4.sub(SIMD.float32x4.mul(row2, tmp1), minor3);
+      tmp1 = SIMD.float32x4.shuffle(tmp1, SIMD.ZWXY); // 0x4E = 01001110
+      minor2 = SIMD.float32x4.sub(SIMD.float32x4.mul(row3, tmp1), minor2);
+      minor3 = SIMD.float32x4.sub(minor3, SIMD.float32x4.mul(row2, tmp1));
+
+      // ----
+      tmp1 = SIMD.float32x4.mul(row0, row3);
+      tmp1 = SIMD.float32x4.shuffle(tmp1, SIMD.YXWZ); // 0xB1 = 10110001
+      minor1 = SIMD.float32x4.sub(minor1, SIMD.float32x4.mul(row2, tmp1));
+      minor2 = SIMD.float32x4.add(SIMD.float32x4.mul(row1, tmp1), minor2);
+      tmp1 = SIMD.float32x4.shuffle(tmp1, SIMD.ZWXY); // 0x4E = 01001110
+      minor1 = SIMD.float32x4.add(SIMD.float32x4.mul(row2, tmp1), minor1);
+      minor2 = SIMD.float32x4.sub(minor2, SIMD.float32x4.mul(row1, tmp1));
+
+      // ----
+      tmp1 = SIMD.float32x4.mul(row0, row2);
+      tmp1 = SIMD.float32x4.shuffle(tmp1, SIMD.YXWZ); // 0xB1 = 10110001
+      minor1 = SIMD.float32x4.add(SIMD.float32x4.mul(row3, tmp1), minor1);
+      minor3 = SIMD.float32x4.sub(minor3, SIMD.float32x4.mul(row1, tmp1));
+      tmp1 = SIMD.float32x4.shuffle(tmp1, SIMD.ZWXY); // 0x4E = 01001110
+      minor1 = SIMD.float32x4.sub(minor1, SIMD.float32x4.mul(row3, tmp1));
+      minor3 = SIMD.float32x4.add(SIMD.float32x4.mul(row1, tmp1), minor3);
+
+      // Compute determinant
+      det = SIMD.float32x4.mul(row0, minor0);
+      det = SIMD.float32x4.add(SIMD.float32x4.shuffle(det, SIMD.ZWXY), det); // 0x4E = 01001110
+      det = SIMD.float32x4.add(SIMD.float32x4.shuffle(det, SIMD.YXWZ), det); // 0xB1 = 10110001
+      tmp1 = SIMD.float32x4.reciprocal(det);
+      det = SIMD.float32x4.sub(SIMD.float32x4.add(tmp1, tmp1), SIMD.float32x4.mul(det, SIMD.float32x4.mul(tmp1, tmp1)));
+      det = SIMD.float32x4.shuffle(det, SIMD.XXXX);
+
+      // These shuffles aren't necessary if the faulty transposition is done
+      // up at the top of this function.
+      //minor0 = SIMD.float32x4.shuffle(minor0, SIMD.ZYXW);
+      //minor1 = SIMD.float32x4.shuffle(minor1, SIMD.ZYXW);
+      //minor2 = SIMD.float32x4.shuffle(minor2, SIMD.ZYXW);
+      //minor3 = SIMD.float32x4.shuffle(minor3, SIMD.ZYXW);
+
+      // Compute final values by multiplying with 1/det
+      minor0 = SIMD.float32x4.mul(det, minor0);
+      minor1 = SIMD.float32x4.mul(det, minor1);
+      minor2 = SIMD.float32x4.mul(det, minor2);
+      minor3 = SIMD.float32x4.mul(det, minor3);
+
+      dstx4.setAt(0, minor0);
+      dstx4.setAt(1, minor1);
+      dstx4.setAt(2, minor2);
+      dstx4.setAt(3, minor3);
     }
   }
 
   // Non SIMD version of the kernel
   function nonSimdMatrixInverseN(n) {
-    for (var i = 0; i < n; ++i) {
-      nonSimdMatrixInverse ();
+    for (var iterations = 0; iterations < n; ++iterations) {
+      // Transpose the source matrix
+      for (var i = 0; i < 4; i++) {
+        tsrc[i] = src[i * 4];
+        tsrc[i + 4] = src[i * 4 + 1];
+        tsrc[i + 8] = src[i * 4 + 2];
+        tsrc[i + 12] = src[i * 4 + 3];
+      }
+
+      // Calculate pairs for first 8 elements (cofactors)
+      tmp[0] = tsrc[10] * tsrc[15];
+      tmp[1] = tsrc[11] * tsrc[14];
+      tmp[2] = tsrc[9] * tsrc[15];
+      tmp[3] = tsrc[11] * tsrc[13];
+      tmp[4] = tsrc[9] * tsrc[14];
+      tmp[5] = tsrc[10] * tsrc[13];
+      tmp[6] = tsrc[8] * tsrc[15];
+      tmp[7] = tsrc[11] * tsrc[12];
+      tmp[8] = tsrc[8] * tsrc[14];
+      tmp[9] = tsrc[10] * tsrc[12];
+      tmp[10] = tsrc[8] * tsrc[13];
+      tmp[11] = tsrc[9] * tsrc[12];
+
+      // calculate first 8 elements (cofactors)
+      dst[0] = tmp[0] * tsrc[5] + tmp[3] * tsrc[6] + tmp[4] * tsrc[7];
+      dst[0] -= tmp[1] * tsrc[5] + tmp[2] * tsrc[6] + tmp[5] * tsrc[7];
+      dst[1] = tmp[1] * tsrc[4] + tmp[6] * tsrc[6] + tmp[9] * tsrc[7];
+      dst[1] -= tmp[0] * tsrc[4] + tmp[7] * tsrc[6] + tmp[8] * tsrc[7];
+      dst[2] = tmp[2] * tsrc[4] + tmp[7] * tsrc[5] + tmp[10] * tsrc[7];
+      dst[2] -= tmp[3] * tsrc[4] + tmp[6] * tsrc[5] + tmp[11] * tsrc[7];
+      dst[3] = tmp[5] * tsrc[4] + tmp[8] * tsrc[5] + tmp[11] * tsrc[6];
+      dst[3] -= tmp[4] * tsrc[4] + tmp[9] * tsrc[5] + tmp[10] * tsrc[6];
+      dst[4] = tmp[1] * tsrc[1] + tmp[2] * tsrc[2] + tmp[5] * tsrc[3];
+      dst[4] -= tmp[0] * tsrc[1] + tmp[3] * tsrc[2] + tmp[4] * tsrc[3];
+      dst[5] = tmp[0] * tsrc[0] + tmp[7] * tsrc[2] + tmp[8] * tsrc[3];
+      dst[5] -= tmp[1] * tsrc[0] + tmp[6] * tsrc[2] + tmp[9] * tsrc[3];
+      dst[6] = tmp[3] * tsrc[0] + tmp[6] * tsrc[1] + tmp[11] * tsrc[3];
+      dst[6] -= tmp[2] * tsrc[0] + tmp[7] * tsrc[1] + tmp[10] * tsrc[3];
+      dst[7] = tmp[4] * tsrc[0] + tmp[9] * tsrc[1] + tmp[10] * tsrc[2];
+      dst[7] -= tmp[5] * tsrc[0] + tmp[8] * tsrc[1] + tmp[11] * tsrc[2];
+
+      // calculate pairs for second 8 elements (cofactors)
+      tmp[0] = tsrc[2] * tsrc[7];
+      tmp[1] = tsrc[3] * tsrc[6];
+      tmp[2] = tsrc[1] * tsrc[7];
+      tmp[3] = tsrc[3] * tsrc[5];
+      tmp[4] = tsrc[1] * tsrc[6];
+      tmp[5] = tsrc[2] * tsrc[5];
+      tmp[6] = tsrc[0] * tsrc[7];
+      tmp[7] = tsrc[3] * tsrc[4];
+      tmp[8] = tsrc[0] * tsrc[6];
+      tmp[9] = tsrc[2] * tsrc[4];
+      tmp[10] = tsrc[0] * tsrc[5];
+      tmp[11] = tsrc[1] * tsrc[4];
+
+      // calculate second 8 elements (cofactors)
+      dst[8] = tmp[0] * tsrc[13] + tmp[3] * tsrc[14] + tmp[4] * tsrc[15];
+      dst[8] -= tmp[1] * tsrc[13] + tmp[2] * tsrc[14] + tmp[5] * tsrc[15];
+      dst[9] = tmp[1] * tsrc[12] + tmp[6] * tsrc[14] + tmp[9] * tsrc[15];
+      dst[9] -= tmp[0] * tsrc[12] + tmp[7] * tsrc[14] + tmp[8] * tsrc[15];
+      dst[10] = tmp[2] * tsrc[12] + tmp[7] * tsrc[13] + tmp[10] * tsrc[15];
+      dst[10] -= tmp[3] * tsrc[12] + tmp[6] * tsrc[13] + tmp[11] * tsrc[15];
+      dst[11] = tmp[5] * tsrc[12] + tmp[8] * tsrc[13] + tmp[11] * tsrc[14];
+      dst[11] -= tmp[4] * tsrc[12] + tmp[9] * tsrc[13] + tmp[10] * tsrc[14];
+      dst[12] = tmp[2] * tsrc[10] + tmp[5] * tsrc[11] + tmp[1] * tsrc[9];
+      dst[12] -= tmp[4] * tsrc[11] + tmp[0] * tsrc[9] + tmp[3] * tsrc[10];
+      dst[13] = tmp[8] * tsrc[11] + tmp[0] * tsrc[8] + tmp[7] * tsrc[10];
+      dst[13] -= tmp[6] * tsrc[10] + tmp[9] * tsrc[11] + tmp[1] * tsrc[8];
+      dst[14] = tmp[6] * tsrc[9] + tmp[11] * tsrc[11] + tmp[3] * tsrc[8];
+      dst[14] -= tmp[10] * tsrc[11] + tmp[2] * tsrc[8] + tmp[7] * tsrc[9];
+      dst[15] = tmp[10] * tsrc[10] + tmp[4] * tsrc[8] + tmp[9] * tsrc[9];
+      dst[15] -= tmp[8] * tsrc[9] + tmp[11] * tsrc[10] + tmp[5] * tsrc[8];
+
+      // calculate determinant
+      var det = tsrc[0] * dst[0] + tsrc[1] * dst[1] + tsrc[2] * dst[2] + tsrc[3] * dst[3];
+
+      // calculate matrix inverse
+      det = 1 / det;
+      for (var j = 0; j < 16; j++) {
+        dst[j] *= det;
+      }
     }
   }
 
--- src/benchmarks/transpose4x4.js
+++ src/benchmarks/transpose4x4.js
@@ -159,14 +159,52 @@
 
   function simdTransposeN(n) {
     for (var i = 0; i < n; ++i) {
-//      simdTranspose();
-      simdTransposeMix();
+      var src0 = srcx4.getAt(0);
+      var src1 = srcx4.getAt(1);
+      var src2 = srcx4.getAt(2);
+      var src3 = srcx4.getAt(3);
+      var dst0;
+      var dst1;
+      var dst2;
+      var dst3;
+      var tmp01;
+      var tmp23;
+
+      tmp01 = SIMD.float32x4.shuffleMix(src0, src1, SIMD.XYXY);
+      tmp23 = SIMD.float32x4.shuffleMix(src2, src3, SIMD.XYXY);
+      dst0 = SIMD.float32x4.shuffleMix(tmp01, tmp23, SIMD.XZXZ);
+      dst1 = SIMD.float32x4.shuffleMix(tmp01, tmp23, SIMD.YWYW);
+
+      tmp01 = SIMD.float32x4.shuffleMix(src0, src1, SIMD.ZWZW);
+      tmp23 = SIMD.float32x4.shuffleMix(src2, src3, SIMD.ZWZW);
+      dst2 = SIMD.float32x4.shuffleMix(tmp01, tmp23, SIMD.XZXZ);
+      dst3 = SIMD.float32x4.shuffleMix(tmp01, tmp23, SIMD.YWYW);
+
+      dstx4.setAt(0, dst0);
+      dstx4.setAt(1, dst1);
+      dstx4.setAt(2, dst2);
+      dstx4.setAt(3, dst3);
     }
   }
 
   function transposeN(n) {
     for (var i = 0; i < n; ++i) {
-      transpose();
+      dst[0] = src[0];
+      dst[1] = src[4];
+      dst[2] = src[8];
+      dst[3] = src[12];
+      dst[4] = src[1];
+      dst[5] = src[5];
+      dst[6] = src[9];
+      dst[7] = src[13];
+      dst[8] = src[2];
+      dst[9] = src[6];
+      dst[10] = src[10];
+      dst[11] = src[14];
+      dst[12] = src[3];
+      dst[13] = src[7];
+      dst[14] = src[11];
+      dst[15] = src[15];
     }
   }
 
