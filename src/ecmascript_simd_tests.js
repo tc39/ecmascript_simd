6751101cd09bb3e60e548ab32f05f5e6090fc4b1
John McCutchan <john@johnmccutchan.com>
2015-01-07 08:38:08 -0800

    Readd clamp and tests

--- src/ecmascript_simd_tests.js
+++ src/ecmascript_simd_tests.js
@@ -287,6 +287,17 @@ test('float32x4 div', function() {
   equal(i.w, Infinity);
 });
 
+test('float32x4 clamp', function() {
+  var a = SIMD.float32x4(-20.0, 10.0, 30.0, 0.5);
+  var lower = SIMD.float32x4(2.0, 1.0, 50.0, 0.0);
+  var upper = SIMD.float32x4(2.5, 5.0, 55.0, 1.0);
+  var c = SIMD.float32x4.clamp(a, lower, upper);
+  equal(2.0, c.x);
+  equal(5.0, c.y);
+  equal(50.0, c.z);
+  equal(0.5, c.w);
+});
+
 test('float32x4 min', function() {
   var a = SIMD.float32x4(-20.0, 10.0, 30.0, 0.5);
   var lower = SIMD.float32x4(2.0, 1.0, 50.0, 0.0);
@@ -1358,6 +1369,29 @@ test('float64x2 div', function() {
   equal(i0.y, -Infinity);
   equal(i1.x, -Infinity);
   equal(i1.y, Infinity);
+});
+
+test('float64x2 clamp', function() {
+  var a = SIMD.float64x2(-20.0, 10.0);
+  var b = SIMD.float64x2(2.125, 3.0);
+  var lower = SIMD.float64x2(2.0, 1.0);
+  var upper = SIMD.float64x2(2.5, 5.0);
+  var c = SIMD.float64x2.clamp(a, lower, upper);
+  equal(2.0, c.x);
+  equal(5.0, c.y);
+  c = SIMD.float64x2.clamp(b, lower, upper);
+  equal(2.125, c.x);
+  equal(3.0, c.y);
+  a = SIMD.float64x2(-3.4e200, 3.4e250);
+  b = SIMD.float64x2(3.4e100, 3.4e200);
+  lower = SIMD.float64x2(3.4e50, 3.4e100);
+  upper = SIMD.float64x2(3.4e150, 3.4e300);
+  c = SIMD.float64x2.clamp(a, lower, upper);
+  equal(3.4e50, c.x);
+  equal(3.4e250, c.y);
+  c = SIMD.float64x2.clamp(b, lower, upper);
+  equal(3.4e100, c.x);
+  equal(3.4e200, c.y);
 });
 
 test('float64x2 min', function() {
