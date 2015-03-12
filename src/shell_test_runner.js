0706a457005ea4135ed7ab4ac2bf66aa4d230597
Dan Gohman <sunfish@mozilla.com>
2015-03-12 12:13:30 -0700

    Add a simple script for running the tests from a standalone js shell.

---
+++ src/shell_test_runner.js
@@ -0,0 +1,51 @@
+// This is a simple script for running the tests from a standalone JS shell.
+
+load("ecmascript_simd.js");
+
+var currentName = '';
+
+function fail(str)
+{
+  e = Error(str);
+  console.log(e.toString());
+  console.log(e.stack);
+}
+
+function test(name, func) {
+  currentName = name;
+  try {
+    func();
+  } catch (e) {
+    fail('exception thrown from ' + currentName);
+  }
+}
+
+function equal(a, b) {
+  if (a == b)
+    return;
+  fail('equal(' + a + ', ' + b + ') failed in ' + currentName);
+}
+
+function notEqual(a, b) {
+  if (a != b)
+    return;
+  fail('notEqual(' + a + ', ' + b + ') failed in ' + currentName);
+}
+
+function throws(func) {
+  var pass = false;
+  try {
+    func();
+  } catch (e) {
+    pass = true;
+  }
+  if (!pass)
+    fail('throws failed in ' + currentName);
+}
+
+function ok(x) {
+  if (!x)
+    fail('not ok in ' + currentName);
+}
+
+load("ecmascript_simd_tests.js");
